aa2172558dcc6b9f719460a96b7707de8fdde5ed
soumi <soumi@FATHIMANOORA>
2025-11-30 06:59:33 +0530

    Initial Commit

--- AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumBasics/WebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumBasics;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebElementCommands extends Base {
 public void verifyWebElementCommands() {
@@ -9,6 +17,13 @@ public void verifyWebElementCommands() {
 	WebElement messagetextbox=driver.findElement(By.id("single-input-field"));
 	messagetextbox.sendKeys("Fathima");
 	WebElement showMessagebutton=driver.findElement(By.id("button-one"));
+	WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+	wait.until(ExpectedConditions.elementToBeClickable(showMessagebutton));
+	
+	Wait<WebDriver> fluentWait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+	.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+	fluentWait.until(ExpectedConditions.elementToBeClickable(showMessagebutton));
+	
 	showMessagebutton.click();
 	System.out.println(showMessagebutton.isDisplayed());
 	System.out.println(showMessagebutton.isEnabled());
--- AutomationCourse/src/test/java/testscripts/LoginTest.java
+++ AutomationCourse/src/test/java/testscripts/LoginTest.java
@@ -2,8 +2,6 @@ package testscripts;
 
 import java.io.IOException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
